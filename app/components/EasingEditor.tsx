ff351467790b01d1656853d1f73f10ee7dfd9073
Bryan Pak <bryan.m.pak@gmail.com>
2023-11-02 01:01:56 +0900

    basic handle component finished

--- app/components/EasingEditor.tsx
+++ app/components/EasingEditor.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import React, { KeyboardEvent, useMemo, useRef, useState } from "react"
 import Handle from "./Handle"
+import { Position, usePosition } from "../utils/usePosition"
 
 export type EasingEditorValue = [number, number, number, number]
 
@@ -20,6 +21,27 @@ const EasingEditor = ({
   const editorRef = useRef<SVGSVGElement | null>(null)
   const [isDragging, setIsDragging] = useState<null | number>(null)
 
+  const { x, y } = useMemo(() => {
+    const xFunc = (value: number) => Math.round(value * width)
+    const yFunc = (value: number) => Math.round((1 - value) * height)
+
+    return { x: xFunc, y: yFunc }
+  }, [width, height])
+
+  const position = useMemo(
+    () => ({
+      x: {
+        from: x(0),
+        to: x(1),
+      },
+      y: {
+        from: y(0),
+        to: y(1),
+      },
+    }),
+    [x, y]
+  )
+
   const handleMouseMove = (e: React.MouseEvent<SVGSVGElement>) => {
     if (isDragging !== null && editorRef.current) {
       const rect = editorRef.current.getBoundingClientRect()
@@ -35,6 +57,43 @@ const EasingEditor = ({
       }
     }
   }
+
+  // optional: maybe figure this out later
+  // const handleKeyDown = (e: KeyboardEvent<SVGCircleElement>) => {
+  //   if (isDragging !== null && editorRef.current) {
+  //     // Get the current position of the selected handle
+  //     const currentIndexX = isDragging * 2
+  //     const currentIndexY = isDragging * 2 + 1
+  //     let x = value[currentIndexX]
+  //     let y = value[currentIndexY]
+
+  //     switch (e.key) {
+  //       case "ArrowUp":
+  //         y = Math.max(0, y - 5 / height) // Ensure y remains within bounds [0, 1]
+  //         break
+  //       case "ArrowDown":
+  //         y = Math.min(1, y + 5 / height) // Ensure y remains within bounds [0, 1]
+  //         break
+  //       case "ArrowLeft":
+  //         x = Math.max(0, x - 5 / width) // Ensure x remains within bounds [0, 1]
+  //         break
+  //       case "ArrowRight":
+  //         x = Math.min(1, x + 5 / width) // Ensure x remains within bounds [0, 1]
+  //         break
+  //       default:
+  //         return
+  //     }
+
+  //     // Create a new value array with the updated position of the selected handle
+  //     const newValue = value.slice()
+  //     newValue[currentIndexX] = x
+  //     newValue[currentIndexY] = y
+
+  //     // Update the value state
+  //     setValue(newValue as EasingEditorValue)
+  //     console.log(value)
+  //   }
+  // }
 
   const handleMouseDown = (index: number) => () => {
     setIsDragging(index)
@@ -55,15 +114,19 @@ const EasingEditor = ({
     >
       <Handle
         index={0}
-        initialX={value[0] * width}
-        initialY={(1 - value[1]) * height}
-        onMouseDown={handleMouseDown(0)}
+        initialX={value[0]}
+        initialY={value[1]}
+        handleMouseDown={handleMouseDown(0)}
+        position={position}
+        // handleKeyDown={handleKeyDown}
       />
       <Handle
         index={1}
-        initialX={value[2] * width}
-        initialY={(1 - value[3]) * height}
-        onMouseDown={handleMouseDown(1)}
+        initialX={value[2]}
+        initialY={value[3]}
+        handleMouseDown={handleMouseDown(1)}
+        position={position}
+        // handleKeyDown={handleKeyDown}
       />
     </svg>
   )
--- app/components/Handle.tsx
+++ app/components/Handle.tsx
@@ -1,53 +1,55 @@
 "use client"
 
 import React, { KeyboardEvent, useEffect, useRef, useState } from "react"
+import { Position, usePosition } from "../utils/usePosition"
 
 type HandleProps = {
   index: number
   initialX: number
   initialY: number
-  onMouseDown: () => void
+  handleMouseDown: () => void
+  position: Position
+  // handleKeyDown: (e: KeyboardEvent<SVGCircleElement>) => void
 }
 
-const Handle = ({ initialX, initialY, onMouseDown }: HandleProps) => {
-  const [position, setPosition] = useState({ x: initialX, y: initialY })
-
-  const handleKeyDown = (e: KeyboardEvent<SVGCircleElement>) => {
-    let { x, y } = position
-    switch (e.key) {
-      case "ArrowUp":
-        y -= 5 // Adjust the value as per your requirement
-        break
-      case "ArrowDown":
-        y += 5 // Adjust the value as per your requirement
-        break
-      case "ArrowLeft":
-        x -= 5 // Adjust the value as per your requirement
-        break
-      case "ArrowRight":
-        x += 5 // Adjust the value as per your requirement
-        break
-      default:
-        break
-    }
-    setPosition({ x, y })
-  }
-
-  useEffect(() => {
-    setPosition({ x: initialX, y: initialY })
-  }, [initialX, initialY])
+const Handle = ({
+  index,
+  initialX,
+  initialY,
+  handleMouseDown,
+  position,
+}: // handleKeyDown,
+HandleProps) => {
+  // Calculations for the line's starting point
+  const { x, y } = usePosition(position)
+  // incorrect since you need the usePosition (interpolating between the two points)
+  const startX = x(index)
+  const startY = y(index)
+  const controlX = x(initialX)
+  const controlY = y(initialY)
+  const a = Math.atan2(controlY - startY, controlX - startX)
+  const cxs = controlX - 6 * Math.cos(a)
+  const cys = controlY - 6 * Math.sin(a)
 
   return (
     <>
-      <line />
+      <line
+        stroke='purple'
+        strokeWidth={2}
+        x1={cxs}
+        y1={cys}
+        x2={startX}
+        y2={startY}
+      />
       <circle
         className='focus:rounded-full'
-        cx={position.x}
-        cy={position.y}
+        cx={controlX}
+        cy={controlY}
         r={6}
         fill='purple'
-        onMouseDown={onMouseDown}
-        onKeyDown={handleKeyDown}
+        onMouseDown={handleMouseDown}
+        // optional: maybe figure this out later
+        // onKeyDown={handleKeyDown}
         tabIndex={0}
       />
     </>
