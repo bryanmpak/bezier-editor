e5177e00da76934403264411714b79d7249d12e8
Bryan Pak <bryan.m.pak@gmail.com>
2023-11-01 11:52:47 +0900

    first commit

---
+++ app/components/BezierEditor.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const BezierEditor = () => {
+  return <div></div>
+}
+
+export default BezierEditor
---
+++ app/utils/usePosition.ts
@@ -0,0 +1,26 @@
+import { useMemo } from "react"
+const interp = (a: number, b: number, x: number) => {
+  return a * (1 - x) + b * x
+}
+
+export interface Position {
+  x: {
+    from: number
+    to: number
+  }
+  y: {
+    from: number
+    to: number
+  }
+}
+
+export const usePosition = (p: Position) => {
+  const result = useMemo(
+    () => ({
+      x: (value: number) => Math.round(interp(p.x.from, p.x.to, value)),
+      y: (value: number) => Math.round(interp(p.y.from, p.y.to, value)),
+    }),
+    [p.x.from, p.x.to, p.y.from, p.y.to]
+  )
+  return result
+}
